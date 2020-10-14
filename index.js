6bc10711f524b3d7864397c1e58776592b2d3693
fe2dback <xnqppskxn@naver.com>
2020-10-14 15:33:12 +0900

    sv7

--- index.js
+++ index.js
@@ -41,17 +41,20 @@ client.on('message', (message) => {
   if(message.content == '!탈콥') {
     return message.reply('https://tarkov-market.com/');
   }
-  if(message.content == '페덱스게임') {
-    return message.reply('게임리스트 \n https://drive.google.com/file/d/1CFhcfdCc-B55Zq2Uh6Ebt63yptTZDcYL/view?usp=sharing');
-   
+  if(message.content == '페덱스') {
+    return message.reply('왜불러시발');
+  }
+  if(message.content == '페덱스 항아리 게임') {
+    return message.reply('https://drive.google.com/file/d/1CFhcfdCc-B55Zq2Uh6Ebt63yptTZDcYL/view?usp=sharing');
+  }
   
 
   if(message.content == 'embed') {
-    let img = 'https://cdn.discordapp.com/attachments/582839803452588092/764778947509878824/dzz.PNG';
+    let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
     let embed = new Discord.RichEmbed()
       .setTitle('타이틀')
       .setURL('http://www.naver.com')
-      .setAuthor('fe2dback', img, 'http://www.naver.com')
+      .setAuthor('나긋해', img, 'http://www.naver.com')
       .setThumbnail(img)
       .addBlankField()
       .addField('Inline field title', 'Some value here')
@@ -61,22 +64,22 @@ client.on('message', (message) => {
       .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
       .addBlankField()
       .setTimestamp()
-      .setFooter('fe2dback', img)
+      .setFooter('나긋해가 만듬', img)
 
     message.channel.send(embed)
   } else if(message.content == '!help') {
     let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
     let commandList = [
       {name: '!help', desc: 'help'},
-      //{name: 'ping', desc: '현재 핑 상태'},
       {name: '!탈콥', desc: '타르코프 게임아이템 시세조회'},
       {name: '!옵지', desc: 'OP.GG'},
+      //{name: 'ping', desc: '현재 핑 상태'},
       {name: 'embed', desc: 'embed 예제1'},
       {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
-      //{name: '!전체공지2', desc: 'dm으로 전체 embed 형식으로 공지 보내기'},
+      {name: '!전체공지2', desc: 'dm으로 전체 embed 형식으로 공지 보내기'},
       {name: '!청소', desc: '텍스트 지움'},
       {name: '!초대코드', desc: '해당 채널의 초대 코드 표기'},
-      //{name: '!초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
+      {name: '!초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
       
     ];
     let commandStr = '';
@@ -223,5 +226,3 @@ async function AutoMsgDelete(message, str, delay = 3000) {
 
 
 client.login(token);
-
-//https://dashboard.heroku.com/apps/fe2dbackbot/deploy/github
