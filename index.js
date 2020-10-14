2e68d584607f8a468ff7f1f66cf02bf51f23b450
fe2dback <xnqppskxn@naver.com>
2020-10-14 14:58:16 +0900

    sv6

--- index.js
+++ index.js
@@ -42,9 +42,8 @@ client.on('message', (message) => {
     return message.reply('https://tarkov-market.com/');
   }
   if(message.content == '페덱스게임') {
-    return message.reply('게임리스트');
-    return message.reply('https://drive.google.com/file/d/1CFhcfdCc-B55Zq2Uh6Ebt63yptTZDcYL/view?usp=sharing');
-  }
+    return message.reply('게임리스트 \n https://drive.google.com/file/d/1CFhcfdCc-B55Zq2Uh6Ebt63yptTZDcYL/view?usp=sharing');
+   
   
 
   if(message.content == 'embed') {
