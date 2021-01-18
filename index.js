4f85250260054689be223a8399a9f9884d82bbe0
fe2dback <66859338+fe2dback@users.noreply.github.com>
2021-01-18 09:57:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ client.on('message', (message) => {
       //{name: 'embed', desc: 'embed 예제1'},
       {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
      // {name: '!전체공지2', desc: 'dm으로 전체 embed 형식으로 공지 보내기'},
-      {name: '!clear', desc: '텍스트 지움'},
+     // {name: '!clear', desc: '텍스트 지움'},
       {name: '!초대코드', desc: '해당 채널의 초대 코드 표기'},
      // {name: '!초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
     ];
